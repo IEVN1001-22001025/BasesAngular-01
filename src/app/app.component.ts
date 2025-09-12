a9df39605aa0e0bec4f517affdfe911d0d5620bf
Abril Samantha Lopez Ruiz <samanthalopezruiz5382@gmail.com>
2025-09-11 21:05:39 -0600

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
+  selector: 'app-root', // es un alias con el que se manda a llamar al componente
+  templateUrl: './app.component.html',//con url se invoca al archivo que va componer
+  standalone: false,// esta apagado
+  styleUrl: './app.component.css'// apunta al archivo de los estilos
 })
 export class AppComponent {
-  title = 'basesAngular01';
+  title = ' Sam basesAngular01 ';
+  duplicarNumero(valor:number):number{
+    return valor*2
+  }
+  pelicula={
+    titulo:'El padrino',
+    anio:1972,
+    genero:"Crimen/Drama"
+  }
 }
